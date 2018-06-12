defc5f62af120e33000aec5386c67a2310221ce9
efri-yang <948061564@qq.com>
2018-06-12 09:47:12 +0800

    c

--- tests/02.php
+++ tests/02.php
@@ -1,7 +1,35 @@
 <?php
-$a = 10;
-$b = 20;
+class A {
+    public $foo = 1;
+} 
 
-list($a, $b) = array($b, $a);
-echo $b;
+$a = new A;
+$b = $a;
+
+
+$b->foo = 2;
+echo $a->foo."\n";
+
+
+
+$c = new A;
+$d = &$c;    // $c ,$d是引用
+             // ($c,$d) = <id>
+
+$d->foo = 2;
+echo $c->foo."\n";
+
+
+$e = new A;
+
+function foo($obj) {
+    // ($obj) = ($e) = <id>
+    $obj->foo = 2;
+}
+
+foo($e);
+echo $e->foo."\n";
+
+
+
 ?>

f9fde1bde9f4dc4c9ded34ac40b3ed49e50a8363
efri-yang <948061564@qq.com>
2018-06-14 09:12:29 +0800

    c

--- tests/02.php
+++ tests/02.php
@@ -1,32 +1,39 @@
 <?php
-class Address{
-	protected $city;
-	public function setCity($city){
-		$this->city=$city;
-	}
-	public function getCity(){
-		return $this->city;
-	}
+class Address {
+    protected $city;
+    public function setCity($city) {
+        $this->city = $city;
+    }
+    public function getCity() {
+        return $this->city;
+    }
 }
 
 class Person {
-	protected $name;
-	protected $address;
-	public function __construct(){
-		$this->address=new Address();
-	}
-	public function setName($name){
-		$this->name=$name;
-	}
-	public function getName(){
-		return $this->name;
-	}
+    protected $name;
+    protected $address;
+    public function __construct() {
+        $this->address = new Address();
+    }
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function getName() {
+        return $this->name;
+    }
 
-	public function __call($method,$arguments){
-		if(method_exists($this->address,$method)){
-			return call_user_func_array(function, param_arr)
-		}
-	}
+    public function __call($method, $arguments) {
+        if (method_exists($this->address, $method)) {
+            return call_user_func_array(array($this->address, $method), $arguments);
+        }
+    }
 }
+
+$p1 = new Person();
+
+$p1->setName("ccccyyh");
+$p1->setCity('Sunnyvale');
+
+echo $p1->getCity();
 
 ?>

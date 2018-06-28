d08d1ec626ee651a68fa142d629d15f99b33928f
efri-yang <948061564@qq.com>
2018-06-28 13:31:06 +0800

    原始数据备份

--- sql/原始初始化数据/think_auth_group.sql
+++ sql/原始初始化数据/think_auth_group.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 2018-06-27 02:37:58
+-- Generation Time: 2018-06-28 07:30:44
 -- 服务器版本： 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -33,7 +33,7 @@ CREATE TABLE `think_auth_group` (
   `title` char(100) NOT NULL DEFAULT '',
   `description` varchar(100) NOT NULL,
   `status` tinyint(1) NOT NULL DEFAULT '1',
-  `rules` char(80) NOT NULL DEFAULT ''
+  `rules` varchar(100) NOT NULL DEFAULT ''
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
@@ -41,9 +41,9 @@ CREATE TABLE `think_auth_group` (
 --
 
 INSERT INTO `think_auth_group` (`id`, `title`, `description`, `status`, `rules`) VALUES
-(1, '普通管理员', '', 1, '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,28,29,31,34,33'),
-(2, '普通管理员', '', 1, '1,2,12,13,14,15,23,28,29,31,34,40,35,36,37,38,39'),
-(3, '用户', '', 1, '1,2,23,28');
+(1, '超级管理员', '', 1, '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33'),
+(2, '普通管理员', '', 1, '1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,23,24,25,26,27,28,29,30,31,32,33'),
+(3, '用户', '', 1, '1,2,23,24,25,26,27,28,29,30,31,32,33,34');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +63,7 @@ ALTER TABLE `think_auth_group`
 -- 使用表AUTO_INCREMENT `think_auth_group`
 --
 ALTER TABLE `think_auth_group`
-  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
